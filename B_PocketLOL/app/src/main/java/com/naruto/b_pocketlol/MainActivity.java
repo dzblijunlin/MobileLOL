32db152f170e682b3ba6a1015e3028095bd0b38d
大嘴宝 <friendforever14@vip.qq.com>
2016-12-19 18:03:24 +0800

    创建B段

---
+++ B_PocketLOL/app/src/main/java/com/naruto/b_pocketlol/MainActivity.java
@@ -0,0 +1,14 @@
+package com.naruto.b_pocketlol;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 我宣布!B段项目现在开始~~~
+    }
+}
